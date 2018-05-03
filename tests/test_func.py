26d7d32ccbc87851c6d0173ccf482ad5e2f8042f
SunnyRainTZ <31945198+SunnyRainTZ@users.noreply.github.com>
2018-05-03 16:51:47 +0800

    Update test_func.py

--- tests/test_func.py
+++ tests/test_func.py
@@ -2,9 +2,9 @@ def add(a, b):
     return a + b
 
 
-def test_add_one(self):
+def test_add_one():
     assert self.add(1, 2) == 3
 
 
-def test_add_two(self):
+def test_add_two():
     assert self.add(3, 5) == 8
